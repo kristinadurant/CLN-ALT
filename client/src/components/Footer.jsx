f68044e42b03361484fc30b3c942792e6706e72f
kristinadurant <kristinadurant.p@gmail.com>
2020-11-28 03:06:23 -0500

    added style for footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,55 +4,45 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer id="footer">
-      <div>
-        <a
-          href="https://www.facebook.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={require('../images/facebook.svg')} alt="facebook" />
-        </a>
-        <a
-          href="https://www.instagram.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={require('../images/instagram.svg')} alt="instagram" />
-        </a>
-        <a
-          href="https://twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={require('../images/twitter.svg')} alt="twitter" />
-        </a>
+      <div className="inner">
+        <div className="social">
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={require('../images/facebook.svg')} alt="facebook" />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={require('../images/instagram.svg')} alt="instagram" />
+          </a>
+          <a
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={require('../images/twitter.svg')} alt="twitter" />
+          </a>
+        </div>
+        <ul>
+          <li>
+            <Link to="/ourStory">ABOUT US</Link>
+          </li>
+          <li>
+            <Link to="#">CONTACT</Link>
+          </li>
+          <li>
+            <Link to="#">PRIVACY LOGIN</Link>
+          </li>
+          <li>
+            <Link to="#">TERMS OF USE</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to="/ourStory">ABOUT US</Link>
-        </li>
-        <li>
-          <Link to="#">ADVERTISE</Link>
-        </li>
-        <li>
-          <Link to="#">PRIVACY LOGIN</Link>
-        </li>
-        <li>
-          <Link to="/blog">HELP & FAQ</Link>
-        </li>
-        <li>
-          <Link to="#">ACCOUNT</Link>
-        </li>
-        <li>
-          <Link to="#">CONTACT</Link>
-        </li>
-        <li>
-          <Link to="#">TERMS OF USE</Link>
-        </li>
-        <li>
-          <Link to="#">SUPPORT</Link>
-        </li>
-      </ul>
     </footer>
   );
 };
