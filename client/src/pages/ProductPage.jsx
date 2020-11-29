d40d4bf8b6e8e401a557092333fc5a89993370c4
kristinadurant <kristinadurant.p@gmail.com>
2020-11-29 15:29:10 -0500

    fixed loading

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -29,67 +29,60 @@ const ProductPage = () => {
   }, [id, setProduct]);
 
   return (
-    <div>
-      {!loading && (
-        <div id="product" className="inner">
-          <h4>{product.title}</h4>
-          <p>{product.description}</p>
-          <div className="productContainer">
-            <ProductImage
-              id={product._id}
-              image={product.image}
-              verified={product.verified}
-              title={product.title}
-            />
-            <div className="tags">
-              {product.tags?.map((tag, index) => {
-                return (
-                  <img
-                    key={index}
-                    src={require(`../images/${tag}.svg`)}
-                    alt={tag}
-                  />
-                );
-              })}
-            </div>
-          </div>
-          {hideMeter && (
-            <Meter
-              ingredients={product.ingredients}
-              verified={product.verified}
-            />
-          )}
-          <div className="tabs">
-            <button
-              className={tab1 && 'active'}
-              onClick={(e) => {
-                setTab1(true);
-              }}
-            >
-              Ingredients
-            </button>
-            <button
-              id="reviews"
-              className={!tab1 && 'active'}
-              onClick={(e) => {
-                setTab1(false);
-              }}
-            >
-              Reviews
-            </button>
-          </div>
-          <div className="ingredients" style={{ display: !tab1 && 'none' }}>
-            {product.ingredients?.map((ingredient) => (
-              <Ingredients key={ingredient._id} ingredient={ingredient} />
-            ))}
-          </div>
-          <div className="reviews" style={{ display: tab1 && 'none' }}>
-            {product?.reviews && (
-              <Reviews reviews={product.reviews} productId={id} />
-            )}
-          </div>
+    <div id="product" className="inner">
+      <h4>{product.title}</h4>
+      <p>{product.description}</p>
+      <div className="productContainer">
+        <ProductImage
+          id={product._id}
+          image={product.image}
+          verified={product.verified}
+          title={product.title}
+        />
+        <div className="tags">
+          {product.tags?.map((tag, index) => {
+            return (
+              <img
+                key={index}
+                src={require(`../images/${tag}.svg`)}
+                alt={tag}
+              />
+            );
+          })}
         </div>
+      </div>
+      {hideMeter && (
+        <Meter ingredients={product.ingredients} verified={product.verified} />
       )}
+      <div className="tabs">
+        <button
+          className={tab1 && 'active'}
+          onClick={(e) => {
+            setTab1(true);
+          }}
+        >
+          Ingredients
+        </button>
+        <button
+          id="reviews"
+          className={!tab1 && 'active'}
+          onClick={(e) => {
+            setTab1(false);
+          }}
+        >
+          Reviews
+        </button>
+      </div>
+      <div className="ingredients" style={{ display: !tab1 && 'none' }}>
+        {product.ingredients?.map((ingredient) => (
+          <Ingredients key={ingredient._id} ingredient={ingredient} />
+        ))}
+      </div>
+      <div className="reviews" style={{ display: tab1 && 'none' }}>
+        {product?.reviews && (
+          <Reviews reviews={product.reviews} productId={id} />
+        )}
+      </div>
     </div>
   );
 };
