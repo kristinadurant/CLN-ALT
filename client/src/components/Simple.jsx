4f9a27a8ff9f05b3073a58676fa87e07bbcc6f2d
Amandat97 <mandy_isabel@yahoo.com>
2020-11-29 19:02:27 -0500

    slider

--- client/src/components/Simple.jsx
+++ client/src/components/Simple.jsx
@@ -20,20 +20,14 @@ const responsive = {
   }
 };
 const images = [
-  'https://unsplash.com/photos/TQMRdv6dFrg',
-  'https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-  'https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+  'https://files.willkennedy.dev/cln_alt/charisse-kenion-BgbOGZRmeXs-unsplash.jpg',
+  'https://files.willkennedy.dev/cln_alt/sincerely-media-GteR27txmaM-unsplash.jpg',
+  'https://files.willkennedy.dev/cln_alt/toa-heftiba-TQMRdv6dFrg-unsplash.jpg',
+  'https://files.willkennedy.dev/cln_alt/lina-verovaya-hdvqsYqvdqI-unsplash.jpg',
+  'https://files.willkennedy.dev/cln_alt/onne-beauty-lZ7tao79Y1A-unsplash.jpg',
+  'https://files.willkennedy.dev/cln_alt/toa-heftiba-TQMRdv6dFrg-unsplash.jpg',
+  'https://files.willkennedy.dev/cln_alt/jess-harper-sunday-4zhaPicBe0c-unsplash.jpg'
 ];
-
 const Simple = ({ deviceType }) => {
   return (
     <Carousel
@@ -47,7 +41,7 @@ const Simple = ({ deviceType }) => {
         return (
           <Image
             draggable={false}
-            style={{ width: '95%', height: '100%' }}
+            style={{ width: '80%', height: '90%' }}
             src={image}
           />
         );
